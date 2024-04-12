85f2d6ec12dc06cabc01d38264460565f199e396
Luca Sartore <lucasar.com8@gmail.com>
2024-04-12 21:47:13 +0200

    finish organization manager

--- deliverable_1/Classes.js
+++ deliverable_1/Classes.js
@@ -689,3 +689,78 @@ class userManager extends dataBaseManager{
     deleteGoogleUserInfo(userId){
     }
 }
+
+class organizationManager extends dataBaseManager{
+
+    //////////////////////////// Creation ////////////////////////////
+
+    /**
+     * Create a new organization in the database, the id of the organization will be automatically generated
+     * return the id of the organization created
+     * @param {Organization} organization 
+     * @returns {CustomResponse<number>}
+     */
+    createOrganization(organization){
+    }
+
+    //////////////////////////// Updating ////////////////////////////
+
+    /**
+     * Update the name of the organization with the given id to the new name that is passed
+     * this function will also edit the profile of the user to add the organization to the user's profile
+     * @param {number} organizationId
+     * @param {string} newName
+     * @returns {CustomResponse<void>}
+    */
+    addUserToOrganization(organizationId, userId){
+    }
+
+    /**
+     * Update the license of the organization with the given id to the new license that is passed
+     * @param {number} organizationId
+     * @param {boolean} newLicense
+     * @returns {CustomResponse<void>}
+     */
+    updateLicense(organizationId, newLicense){
+    }
+
+    /**
+     * Update the license expiration date of the organization with the given id to the new date that is passed
+     * @param {number} organizationId
+     * @param {Date} newDate
+     * @returns {CustomResponse<void>}
+     */
+    updateLicenseExpirationDate(organizationId, newDate){
+    }
+
+    //////////////////////////// Deleting ////////////////////////////
+
+    /**
+     * Delete the organization with the given id 
+     * @param {number} organizationId 
+     * @returns {CustomResponse<void>}
+     */
+    deleteOrganization(organizationId){
+    }
+
+    /**
+     * Remove a user from an organization
+     * Can return an error if the user is the owner of the organization
+     * This function will also update the profile of the user remove the organization from the user's profile
+     * @param {number} organizationId
+     * @param {number} userId 
+     * @returns {CustomResponse<void>}
+     */
+    removeUserFromOrganization(organizationId, userId){
+    }
+    
+    //////////////////////////// Reading ///////////////////////////
+
+    /**
+     * Return one single organization
+     * @param {number} organizationId 
+     * @returns {CustomResponse<Organization>}
+     */
+    readOrganization(organizationId){
+    }
+}
